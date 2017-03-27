806cca082d1d383c24fb53f6cf8b3b421814b4c3
ppv77 <poul.dio.later@gmail.com>
2017-03-27 14:47:21 +0300

    bug in params

--- 250_extract_stage.sh
+++ 250_extract_stage.sh
@@ -2,5 +2,5 @@
 . 000_define.sh
 pushd ${new_root}
 #pv $(ls ${Stage3_file}) | ${sudo_cmd} tar xjpf - --xattrs --numeric-owner -C ${new_root}
-${sudo_cmd} tar xjpf - --xattrs --numeric-owner -C ${new_root}
+${sudo_cmd} tar xjpf ${Stage3_file} --xattrs --numeric-owner -C ${new_root}
 popd
