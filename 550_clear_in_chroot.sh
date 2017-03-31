87e1f25d2d687cb34f7b414f6d50b5a0da259f1c
ppv77 <poul.dio.later@gmail.com>
2017-03-31 09:46:29 +0300

    change clean method for kernel sources if precompiled

--- 550_clear_in_chroot.sh
+++ 550_clear_in_chroot.sh
@@ -6,7 +6,7 @@ ${sudo_cmd} chmod +x ${new_root}/clean.sh
 
 printf "cd /usr/src/linux/ && make clean\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
 [ $debug = 1 ] && read -p Enter
-[ $rm_linux_sources = 1 ] && printf "emerge -c linux-sources && emerge -c\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
+[ ( $rm_linux_sources = 1 ) && ( $kernel != "precompiled" ) ] && printf "emerge -c linux-sources && emerge -c\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
 [ $debug = 1 ] && read -p Enter
 ${sudo_cmd} chroot ${new_root}  /clean.sh
 ${sudo_cmd} rm ${new_root}/clean.sh
