5134a6218b38581e20cd70de94237594bdb1b20e
ppv77 <poul.dio.later@gmail.com>
2017-03-31 10:25:05 +0300

    some ch....

--- 201_or-copy-from-for_Stage4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. 000_define.sh
-pushd ${new_root}
-${sudo_cmd} rm -i ${Stage3_file}
-[ -f ${Stage3_file} ] && exit
-${sudo_cmd} cp /var/www/localhost/for_stage4/${Stage3_file} ${new_root}
-popd
