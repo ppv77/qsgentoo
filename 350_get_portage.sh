220ebdc0786d40578d2c7ddd885c72e32b389208
ppv77 <poul.dio.later@gmail.com>
2017-03-31 15:59:30 +0300

    change get portage metod

--- 350_get_portage.sh
+++ 350_get_portage.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/bash -v
 . 000_define.sh
-pushd ${new_root}/usr/
-${sudo_cmd} wget ${portage_zip}
-${sudo_cmd} unzip -q stable.zip
-${sudo_cmd} mv gentoo-stable portage
-popd
-#${sudo_cmd} git clone ${portage_uri} ${new_root}/usr/portage
+
+if [ ! ${portage_zip} = "" ] ; then
+    pushd ${new_root}/usr/
+    ${sudo_cmd} wget -q ${portage_zip}
+    ${sudo_cmd} unzip -q stable.zip
+    ${sudo_cmd} mv gentoo-stable portage
+    pushd portage
+    ${sudo_cmd} git pull
+    popd
+    ${sudo_cmd} rm stable.zip
+    popd
+else
+    ${sudo_cmd} git clone https://github.com/gentoo-mirror/gentoo.git ${new_root}/usr/portage
+fi
