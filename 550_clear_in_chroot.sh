85ad75c95b611f8e92581f0980c7cd17ed4da451
ppv77 <poul.dio.later@gmail.com>
2017-03-31 10:07:05 +0300

    no sources no clean

--- 550_clear_in_chroot.sh
+++ 550_clear_in_chroot.sh
@@ -4,9 +4,8 @@
 ${sudo_cmd} truncate --size 0 ${new_root}/clean.sh
 ${sudo_cmd} chmod +x ${new_root}/clean.sh
 
-printf "cd /usr/src/linux/ && make clean\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
 [ $debug = 1 ] && read -p Enter
-[ ( $rm_linux_sources = 1 ) && ( $kernel != "precompiled" ) ] && printf "emerge -c linux-sources && emerge -c\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
+[ $rm_linux_sources = 1 ] && [ $kernel != "precompiled" ] && printf "cd /usr/src/linux/ && make clean\nemerge -c linux-sources && emerge -c\n" | ${sudo_cmd} tee -a ${new_root}/clean.sh >/dev/null
 [ $debug = 1 ] && read -p Enter
 ${sudo_cmd} chroot ${new_root}  /clean.sh
 ${sudo_cmd} rm ${new_root}/clean.sh
