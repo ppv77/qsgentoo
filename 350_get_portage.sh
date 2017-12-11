2585361620f90504a196e2e5d5227af67e0c1e67
ppv77 <poul.dio.later@gmail.com>
2017-12-11 11:22:08 +0300

    some debug

--- 350_get_portage.sh
+++ 350_get_portage.sh
@@ -5,11 +5,16 @@ printf "Download portage.\n"
 
 if [ ! ${portage_zip} = "" ] ; then
     ${sudo_cmd} mkdir -p ${new_root}/usr/portage/gentoo
+[ $debug = 1 ] && read -p Enter
     pushd ${new_root}/usr/ >/dev/null
     ${sudo_cmd} wget ${quiet} ${portage_zip}
+    [ $debug = 1 ] && read -p Enter
     ${sudo_cmd} unzip ${quiet} stable.zip
+    [ $debug = 1 ] && read -p Enter
     ${sudo_cmd} mv gentoo-stable portage/gentoo
+    [ $debug = 1 ] && read -p Enter
     ${sudo_cmd} rm stable.zip
+    [ $debug = 1 ] && read -p Enter
     popd >/dev/null
 else
     ${sudo_cmd} git clone --depth 1 https://github.com/gentoo-mirror/gentoo.git ${new_root}/usr/portage/gentoo
