ffc4e8a921b8ef0edd51259113dd33ed2222be04
ppv77 <poul.dio.later@gmail.com>
2019-02-06 15:23:03 +0300

    mount disks

--- 150_mount_disk.sh
+++ 150_mount_disk.sh
@@ -2,28 +2,38 @@
 . 000_define.sh
 
 printf "Mount disks.\n"
-#mount root
-${sudo_cmd} mkdir ${verbose} -p /mnt/gentoo
-for (( i=1; i < ${#mp[@]}/4+1; i++ ))
-    do
-        [ "${mp[mountpoint,$i]}" = "/" ] && ${sudo_cmd} mount  ${verbose} ${main_device}$i ${new_root}${mp[mountpoint,$i]}
-    done
+${sudo_cmd} mkdir ${verbose} -p ${new_root}
+
+#find root
+for (( i=1; i < ${#pt[@]}/6+1; i++ ))
+do
+    [ "${pt[mp,$i]}" = "/" ] && ${sudo_cmd} mount ${verbose}  "${main_device}$i" "${new_root}${pt[mp,$i]}"
+done
+
+for (( i=1; i < ${#lv[@]}/4+1; i++ ))
+do
+    [ "${lv[mp,$i]}" = "/" ] && ${sudo_cmd} mount ${verbose}  "/dev/mapper/${vg_name}-${lv[name,$i]}" "${new_root}${lv[mp,$i]}"
+done
+
+
+
 
 #mount other
-for (( i=1; i < ${#mp[@]}/4+1; i++ ))
+for (( i=1; i < ${#pt[@]}/6+1; i++ ))
 do
-    case "${mp[mountpoint,$i]}" in
-	"swap" )
-	    ${sudo_cmd} swapon ${verbose} ${main_device}$i
-	    ;;
-	"/" )
-	    ;;
-	"bios_grub" )
-	    ;;
-	* )
-	    [ ! -d ${new_root}${mp[mountpoint,$i]} ] && ${sudo_cmd} mkdir  ${verbose} -p ${new_root}${mp[mountpoint,$i]}
-	    ${sudo_cmd} mount ${verbose} ${main_device}$i ${new_root}${mp[mountpoint,$i]}
-	    ;;
-    esac
-    
+    [ "${pt[mp,$i]}" = "/" ] && continue
+    [ "${pt[mp,$i]}" = "" ] && continue
+    [ "${pt[mp,$i]}" = "swap" ] && continue
+    [ ! -d ${new_root}${pt[mp,$i]} ] && ${sudo_cmd} mkdir  ${verbose} -p ${new_root}${pt[mp,$i]}
+    ${sudo_cmd} mount ${verbose}  "${main_device}$i" "${new_root}${pt[mp,$i]}"
 done
+
+for (( i=1; i < ${#lv[@]}/4+1; i++ ))
+do
+    [ "${lv[mp,$i]}" = "/" ] && continue
+    [ "${lv[mp,$i]}" = "" ] && continue
+    [ "${lv[mp,$i]}" = "swap" ] && continue
+    [ ! -d ${new_root}${lv[mp,$i]} ] && ${sudo_cmd} mkdir  ${verbose} -p ${new_root}${lv[mp,$i]}
+    ${sudo_cmd} mount ${verbose}  "/dev/mapper/${vg_name}-${lv[name,$i]}" "${new_root}${lv[mp,$i]}"
+done
+
