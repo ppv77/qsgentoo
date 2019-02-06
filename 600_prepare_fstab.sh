a5360b54614c3bdb5d9e0ac7d366a63f7a7c35ef
ppv77 <poul.dio.later@gmail.com>
2019-02-06 16:36:43 +0300

    fstab

--- 600_prepare_fstab.sh
+++ 600_prepare_fstab.sh
@@ -6,39 +6,21 @@ printf "Prepare fstab.\n"
 ${sudo_cmd} mv  ${verbose} ${new_root}/etc/fstab  ${new_root}/etc/fstab.default
 ${sudo_cmd} truncate --size 0 ${new_root}/etc/fstab
 
-for (( i=1; i < ${#mp[@]}/4+1; i++ ))
+
+for (( i=1; i < ${#pt[@]}/6+1; i++ ))
 do
-    [ "${mp[mountpoint,$i]}" == "bios_grub" ] && continue
-#fs
-    printf "${main_device}$i		" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-#dir
-    case "${mp[mountpoint,$i]}" in
-	"swap" )
-	    printf "none		" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-	* )
-	    printf  "${mp[mountpoint,$i]}		" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-    esac
-#type
-    printf "${mp[fs,$i]}		" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-    
-    
-#options
-    case "${mp[mountpoint,$i]}" in
-	"swap" )
-	    printf "sw		0		0" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-	"/" )
-	    printf "noatime		0		1" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-	"/boot" )
-	    printf "noauto,noatime	1		2" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-	* )
-	    printf  "noatime		0		2" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
-	    ;;
-    esac
-    
-    printf "\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
+    [ "${pt[mp,$i]}" = "" ] && continue
+    [ "${pt[mp,$i]}" = "swap" ] && printf "${main_device}$i		none		swap		sw		0	0\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null && continue
+    [ "${pt[mp,$i]}" = "/boot" ] && printf "${main_device}$i		${pt[mp,$i]}	${pt[fs,$i]}		defaults,noatime,noauto		0	2\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null && continue
+    printf "${main_device}$i		${pt[mp,$i]}	${pt[fs,$i]}		noatime		0	1\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
 done
+
+
+for (( i=1; i < ${#lv[@]}/4+1; i++ ))
+do
+    [ "${lv[mp,$i]}" = "" ] && continue
+    [ "${lv[mp,$i]}" = "swap" ] && printf "/dev/${vg_name}/${lv[name,$i]}		none		swap		sw		0	0\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null && continue
+    [ "${lv[mp,$i]}" = "/boot" ] && printf "/dev/${vg_name}/${lv[name,$i}		${lv[mp,$i]}	${lv[fs,$i]}		defaults,noatime,noauto	0	2\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null && continue
+    printf "/dev/${vg_name}/${lv[name,$i]}		${lv[mp,$i]}	${lv[fs,$i]}		noatime		0	1\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
+done
+printf "\n" | ${sudo_cmd} tee -a ${new_root}/etc/fstab >/dev/null
