411482550b82749fe88bf5eb393d83e4b7085468
dio <poul.dio.later@gmail.com>
2019-08-30 16:11:07 +0300

    ch portage target

--- 350_get_portage.sh
+++ 350_get_portage.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 . 000_define.sh
-
 printf "Download portage.\n"
 
 if [ ! ${portage_zip} = "" ] ; then
 #    ${sudo_cmd} mkdir ${verbose} -p ${new_root}/usr/portage/gentoo
-    pushd ${new_root}/usr/ >/dev/null
+    pushd ${new_root}/var/db/repos/ >/dev/null
     ${sudo_cmd} wget -nv ${quiet} ${portage_zip}
     ${sudo_cmd} unzip -q gentoo-stable.zip
-    ${sudo_cmd} mv  ${verbose} gentoo-stable portage
+    ${sudo_cmd} mv  ${verbose} gentoo-stable gentoo
     ${sudo_cmd} rm  ${verbose} gentoo-stable.zip
     popd >/dev/null
 #else
