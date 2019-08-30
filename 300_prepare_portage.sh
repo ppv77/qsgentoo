fe9a8e793de0fe01262c7533ebb5db4251df9f8b
dio <poul.dio.later@gmail.com>
2019-08-30 16:10:23 +0300

    rm old data

--- 300_prepare_portage.sh
+++ 300_prepare_portage.sh
@@ -4,7 +4,6 @@
 printf "Prepare portage.\n"
 
 [ ! -d "${new_root}/etc/portage/repos.conf" ] && ${sudo_cmd} mkdir ${verbose} -p ${new_root}/etc/portage/repos.conf
-#[ ! -f "${new_root}/etc/portage/repos.conf/gentoo.conf" ] && ${sudo_cmd} cp ${chroot_files}/gentoo.conf ${new_root}/etc/portage/repos.conf/
 [ ! -f "${new_root}/etc/portage/repos.conf/gentoo.conf" ] && ${sudo_cmd} cp ${verbose} ${new_root}/usr/share/portage/config/repos.conf ${new_root}/etc/portage/repos.conf/
 
 [ ! -d "${new_root}/etc/portage/package.accept_keywords" ] && ${sudo_cmd} mkdir ${verbose} -p ${new_root}/etc/portage/package.accept_keywords
