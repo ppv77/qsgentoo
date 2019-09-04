f550e016ee290195e09f72b2022646e0f2aba80d
dio <poul.dio.later@gmail.com>
2019-09-04 14:55:34 +0300

    kernel=""

--- 000_define.sh
+++ 000_define.sh
@@ -89,10 +89,10 @@ use_packages=0
 
 #---------------------------------------------------
 #get kernel config from booted system  /proc/config.gz (recomended)
-#kernel=""
+kernel=""
 #or
 #use kernel from livecd (very fast)
-kernel="livecd"
+#kernel="livecd"
 #or
 #kernel config from some qsgentoo files
 #kernel="config-esx-minimal-4.9.16"
